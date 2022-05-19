a89851c18e37c88117ee089a1909bbb32c9987d2
thatITfox <maxthecomputerfox@gmail.com>
2022-05-19 18:51:41 +0700

    made it search for fursuit friday posts

--- get-data.py
+++ get-data.py
@@ -1,5 +1,4 @@
 import tweepy
-
 # open api-keys.txt and grab the keys
 with open('api-keys.txt', 'r') as f:
     keys = f.read().splitlines()
@@ -12,3 +11,10 @@ with open('api-keys.txt', 'r') as f:
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
+
+# get the tweets with #fursuitfriday and make sure it's in english and print the text
+tweets = api.search_tweets(q='#FursuitFriday', count=100, lang='en')
+print(len(tweets))
+for index, tweet in enumerate(tweets):
+    print(f"tweet:{index}")
+    print(tweet.text)
