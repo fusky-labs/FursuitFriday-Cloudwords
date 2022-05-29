eeb09c1176ac32dd33730435eeacd608cc020be9
thatITfox <maxthecomputerfox@gmail.com>
2022-05-29 19:33:42 +0700

    added some filtering of the data

--- get-data.py
+++ get-data.py
@@ -12,9 +12,20 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+tweets_list = []
+
 # get the tweets with #fursuitfriday and make sure it's in english and print the text
-tweets = api.search_tweets(q='#FursuitFriday', count=100, lang='en')
+tweets = api.search_tweets(q='#FursuitFriday', count=1000, lang='en')
 print(len(tweets))
 for index, tweet in enumerate(tweets):
     print(f"tweet:{index}")
     print(tweet.text)
+    tweets_list.append(tweet.text)
+
+# replace new lines and emojis with spaces using regex
+import re
+for index, tweet in enumerate(tweets_list):
+    tweets_list[index] = re.sub(r'\n', ' ', tweet)
+    tweets_list[index] = re.sub(r'[^\x00-\x7F]+', ' ', tweets_list[index])
+
+# TODO: save the tweets to a file, could be in json, or txt
