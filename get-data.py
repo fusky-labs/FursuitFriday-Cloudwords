f3dc84f7676492fcead1c6b0efb89a9ab057a6b7
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-07-16 22:46:43 +0800

    Update get-data.py

--- get-data.py
+++ get-data.py
@@ -1,8 +1,9 @@
+import json
+import os
 import re
+import argparse
 import tweepy
 from dotenv import load_dotenv
-import os
-import json
 
 load_dotenv()
 consumer_key = os.getenv("CONSUMER_KEY")
@@ -14,11 +15,23 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+parser = argparse.ArgumentParser(description="Get data from Twitter")
+parser.add_argument("-n", "--twt-count", type=int, help="Number of tweets to get")
+
+args = parser.parse_args()
+
 tweets_list = []
 
 # get the tweets with #fursuitfriday and make sure it's in english and print the text
-tweets = api.search_tweets(q='#FursuitFriday', count=100, lang='en')
-print(len(tweets))
+
+if args.twt_count:
+    tweets = api.search_tweets(q='#FursuitFriday', count=args.twt_count, lang='en')
+    print(len(tweets))
+
+else:
+    tweets = api.search_tweets(q='#FursuitFriday', count=args.twt_count, lang='en')
+    print(len(tweets))
+
 for index, tweet in enumerate(tweets):
     print(f"tweet:{index}")
     print(tweet.text)
@@ -29,4 +42,4 @@ for i, tweet in enumerate(tweets_list):
     tweets_list[i] = re.sub(r'\n', ' ', tweet)
 
 with open('tweets.json', 'w') as outfile:
-    json.dump({"tweets":tweets_list}, outfile)
+    json.dump({"tweets": tweets_list}, outfile)
